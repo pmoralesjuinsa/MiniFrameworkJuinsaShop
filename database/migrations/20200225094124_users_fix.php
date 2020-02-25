785959eb43c9a0de84412996d3b0ad83f69d7ab7
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-25 10:51:31 +0100

    migrate and fix column error at table users

--- database/migrations/20200225094124_users_fix.php
+++ database/migrations/20200225094124_users_fix.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class Users extends AbstractMigration
+class UsersFix extends AbstractMigration
 {
     /**
      * Change Method.
@@ -32,12 +32,7 @@ class Users extends AbstractMigration
     public function change()
     {
         $users = $this->table('users');
-        $users->addColumn('name', 'string', ['limit' => 80]);
-        $users->addColumn('email', 'string', ['limit' => 80]);
-        $users->addColumn('passwrod', 'string', ['limit' => 100]);
-        $users->addColumn('created_at', 'datetime');
-        $users->addColumn('updated_at', 'datetime', ['null' => true]);
-        $users->create();
-
+        $users->renameColumn('passwrod', 'password');
+        $users->update();
     }
 }

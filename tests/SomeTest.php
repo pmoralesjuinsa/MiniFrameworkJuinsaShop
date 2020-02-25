986be1fa2ec0c4317201f39f33b03b5e8b1481ed
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-25 09:13:07 +0100

    initialize dev tools for testing

---
+++ tests/SomeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Test\TripServiceKata\Trip;
+
+use phpDocumentor\Reflection\Types\Void_;
+use PHPUnit\Framework\TestCase;
+
+class SomeTest extends TestCase
+{
+    /**
+     * @var Some
+     */
+    private $some;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function
+    it_does_something()
+    {
+        $this->fail('This test has not been implemented yet.');
+//        $this->assertTrue(true, "ole");
+    }
+}

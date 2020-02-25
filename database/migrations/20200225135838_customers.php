220f24a8a0782cc80e0011d33f4a18a4097a9ff9
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-25 15:01:46 +0100

    migrate customers

---
+++ database/migrations/20200225135838_customers.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Customers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $users = $this->table('customers');
+        $users->addColumn('name', 'string', ['limit' => 80]);
+        $users->addColumn('email', 'string', ['limit' => 80]);
+        $users->addColumn('phone', 'string', ['limit' => 13]);
+        $users->addColumn('address', 'string', ['limit' => 100]);
+        $users->addColumn('created_at', 'datetime');
+        $users->addColumn('updated_at', 'datetime', ['null' => true]);
+        $users->create();
+    }
+}

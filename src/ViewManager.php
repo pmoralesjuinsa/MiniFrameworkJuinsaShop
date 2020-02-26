8fc040d8cf5cb4906b9428ae1f5dedf1aaf984e8
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-26 14:33:58 +0100

    RED: trying to get name of customer logged at template.twig.html

--- src/ViewManager.php
+++ src/ViewManager.php
@@ -8,29 +8,34 @@ class ViewManager
 {
 
     private $twig;
+
     public function __construct()
     {
-        $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__).'/templates');
+        $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/templates');
         $this->twig = new \Twig\Environment($loader, [
-            'cache'=>dirname(__DIR__).'/cache/views'
+            'cache' => dirname(__DIR__) . '/cache/views'
         ]);
     }
 
-    public function render($view, $args=[])
+    public function render($view, $args = [])
     {
-        if($args !=null){
-            extract ($args, EXTR_SKIP);
+        if ($args != null) {
+            extract($args, EXTR_SKIP);
         }
-        $file = dirname(__DIR__).'/templates/'.$view;
-        if( is_readable($file)){
+        $file = dirname(__DIR__) . '/templates/' . $view;
+        if (is_readable($file)) {
             require $file;
-        }else{
+        } else {
             throw  new \InvalidArgumentException();
         }
 
     }
 
-    public function renderTemplate($template, $args=[]){
+    public function renderTemplate($template, $args = [])
+    {
+        $customerAuthed = (array)getAuthenticatedCustomer();
+        array_merge($args, $customerAuthed);
+//        \Kint::dump(getAuthenticatedCustomer());
         echo $this->twig->render($template, $args);
     }
 

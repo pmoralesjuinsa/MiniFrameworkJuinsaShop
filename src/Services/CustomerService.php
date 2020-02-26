ca0bbb99c3032ee3599236817e29f172e5b352bc
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-26 15:40:50 +0100

    BLUE: deprecated CustomerRepository at repositories dir

--- src/Services/CustomerService.php
+++ src/Services/CustomerService.php
@@ -5,27 +5,18 @@ namespace Juinsa\Services;
 
 
 use Juinsa\db\entities\Customer;
-use Juinsa\db\repositories\CustomerRepository;
-use Juinsa\DoctrineManager;
-use Juinsa\LogManager;
 
 class CustomerService extends Service
 {
-    /**
-     * @var CustomerRepository
-     */
-    private CustomerRepository $customerRepository;
-
-    public function __construct(DoctrineManager $doctrineManager, LogManager $logManagaer)
-    {
-        parent::__construct($doctrineManager, $logManagaer);
-
-        $this->customerRepository = new CustomerRepository($this->doctrineManager->em);
-    }
 
     public function login(Customer $customer): ?Customer
     {
-        return $this->customerRepository->getCustomer($customer);
+        return $this->doctrineManager->em->getRepository(Customer::class)->findOneBy(
+            array(
+                'email' => $customer->email,
+                'password' => $customer->password
+            )
+        );
 
     }
 

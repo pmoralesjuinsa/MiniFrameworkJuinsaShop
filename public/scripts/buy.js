2d4205734d28337d4b9a0c43d89dd19e321a44b7
Pedro Morales <pedromorales@grupojuinsa.es>
2020-03-02 16:59:17 +0100

    RED: ajax add to cart

--- public/scripts/buy.js
+++ public/scripts/buy.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-    $('.buy_product').click(function(){
-        alert("hola");
-        return true;
+    $('form.buy_product').on("submit", function(event){
+        event.preventDefault();
+
+        $.ajax({
+            method: "POST",
+            url: "/add_to_cart",
+            data: { cart : $(this).serialize() }
+        })
+            .done(function( numberItems ) {
+                $('#carrito-count').textContent(1);
+            })
+            .fail(function ( msg ) {
+                alert(msg);
+            });
     })
 })

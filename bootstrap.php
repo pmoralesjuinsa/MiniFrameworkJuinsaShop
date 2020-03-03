e643c8e8e064ce613cbdaf0bee2f605ef6e8c6d9
Pedro Morales <pedromorales@grupojuinsa.es>
2020-03-03 17:31:55 +0100

    RED: pay logic with OrderService, controller and view. Doctrine Console files created

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+date_default_timezone_set("Europe/Madrid"); // Set the default timezone
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(- 1);
+
+include 'vendor/autoload.php';
---
+++ cli-config.php
@@ -0,0 +1,28 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+require 'bootstrap.php';
+
+// Paths to Entities that we want Doctrine to see
+$paths = array(
+    "module/Module/src/Entity",
+    "module/MyApplication/src/Entity"
+);
+
+// Tells Doctrine what mode we want
+$isDevMode = true;
+
+// Doctrine connection configuration
+$dbParams = array(
+    'driver' => 'pdo_mysql',
+    'host' => 'juinsa_shop_db',
+    'user' => 'user',
+    'password' => 'password',
+    'dbname' => 'juinsa_shop'
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+return ConsoleRunner::createHelperSet($entityManager);

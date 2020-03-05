cc6c3b6b20137a394c543f3430bed6ed56c583c6
Pedro Morales <pedromorales@grupojuinsa.es>
2020-03-05 17:28:43 +0100

    RED: fix EAV product and attributes tables

--- src/db/entities/ProductTypeAttribute.php
+++ src/db/entities/ProductTypeAttribute.php
@@ -20,77 +20,6 @@ class ProductTypeAttribute
     protected $id;
 
     /**
-     * @ORM\Column(type="string")
-     */
-    protected $value;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="ProductType", inversedBy="attributes")
-     * @ORM\JoinColumn(name="id_product_type", referencedColumnName="id")
-     */
-    protected $product_type;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="ProductAttribute", inversedBy="values")
-     * @ORM\JoinColumn(name="id_product_attribute", referencedColumnName="id")
-     */
-    protected $product_attribute;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="Product", inversedBy="attributes")
-     * @ORM\JoinColumn(name="id_product", referencedColumnName="id")
-     */
-    protected $product;
-
-    /**
-     * @return mixed
-     */
-    public function getProductType()
-    {
-        return $this->product_type;
-    }
-
-    /**
-     * @param mixed $product_type
-     */
-    public function setProductType($product_type): void
-    {
-        $this->product_type = $product_type;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getProductAttribute()
-    {
-        return $this->product_attribute;
-    }
-
-    /**
-     * @param mixed $product_attribute
-     */
-    public function setProductAttribute($product_attribute): void
-    {
-        $this->product_attribute = $product_attribute;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getProduct()
-    {
-        return $this->product;
-    }
-
-    /**
-     * @param mixed $product
-     */
-    public function setProduct($product): void
-    {
-        $this->product = $product;
-    }
-
-    /**
      * @ORM\Column(type="datetime")
      */
     protected $created_at;
@@ -100,25 +29,10 @@ class ProductTypeAttribute
      */
     protected $updated_at;
 
+
     public function __construct()
     {
         $this->created_at = new \DateTime('now');
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getValue()
-    {
-        return $this->value;
-    }
-
-    /**
-     * @param mixed $value
-     */
-    public function setValue($value): void
-    {
-        $this->value = $value;
     }
 
     /**
@@ -168,5 +82,7 @@ class ProductTypeAttribute
     {
         $this->updated_at = $updated_at;
     }
+
+
 
 }

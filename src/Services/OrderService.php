e3c5b0ce7be5117b6dd12f6f926ad9e8e01d8b88
Pedro Morales <pedromorales@grupojuinsa.es>
2020-03-05 13:58:17 +0100

    BLUE: save order and orderlines

--- src/Services/OrderService.php
+++ src/Services/OrderService.php
@@ -9,17 +9,17 @@ use Juinsa\db\entities\Order;
 
 class OrderService extends Service
 {
-    public function createOrder(Order $order)
+    public function createOrder(Order $order): Order
     {
         try {
             $this->doctrineManager->em->persist($order);
             $this->doctrineManager->em->flush();
 
-            return true;
+            return $order;
         } catch (\Exception $e) {
             $this->logManagaer->error($e->getMessage());
         }
 
-        return false;
+        return null;
     }
 }

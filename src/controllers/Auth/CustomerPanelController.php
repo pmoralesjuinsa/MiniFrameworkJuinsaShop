00bf5a235661f95d6285a34ddce25d5b2e7b4e85
Pedro Morales <pedromorales@grupojuinsa.es>
2020-03-05 16:03:29 +0100

    BLUE: customer panel

--- src/controllers/Auth/CustomerPanelController.php
+++ src/controllers/Auth/CustomerPanelController.php
@@ -5,19 +5,11 @@ namespace Juinsa\controllers\Auth;
 
 
 use DI\Annotation\Inject;
-use Juinsa\db\entities\Customer;
-use Juinsa\Services\CustomerService;
 use Juinsa\controllers\Controller;
 use Juinsa\Services\OrderService;
 
 class CustomerPanelController extends Controller
 {
-    /**
-     * @Inject
-     * @var CustomerService
-     */
-    private CustomerService $customerService;
-
     /**
      * @Inject
      * @var OrderService

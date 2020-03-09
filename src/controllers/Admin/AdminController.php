cd0722a65f543d4b88f96e54aa2538a18edd66d6
Pedro Morales <pedromorales@grupojuinsa.es>
2020-03-09 12:14:32 +0100

    RED: backend, login user

--- src/controllers/Admin/AdminController.php
+++ src/controllers/Admin/AdminController.php
@@ -5,19 +5,17 @@ namespace Juinsa\controllers\Admin;
 
 use Juinsa\controllers\Controller;
 use DI\Annotation\Inject;
+use Juinsa\ViewManager;
 
 class AdminController extends Controller
 {
-//    /**
-//     * @Inject
-//     * @var UserService
-//     */
-//    protected UserService $userService;
+
+    protected $adminMenu;
 
     public function index()
     {
         $this->redirectIfNotLoguedAsUser();
 
-        $this->myRenderTemplate('user/admin.twig.html');
+        $this->myRenderTemplate('admin/admin.twig.html');
     }
 }
